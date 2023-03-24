45fd14c1a237dc3708dc7eb1ce81aa5958f0a50d
Louis Anh Tran <louistrannamanh@gmail.com>
2023-03-24 20:20:55 +0800

    louis initialize project

---
+++ app/src/main/java/com/example/Groopr/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.Groopr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+    private DatabaseReference mDatabase;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        mDatabase.child("Nam Anh").setValue("1005242");
+
+
+
+
+
+    }
+}
