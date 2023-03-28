2a1c973aa369690bbd252892e7a1bd7c2ab897c0
Louis Anh Tran <louistrannamanh@gmail.com>
2023-03-29 02:24:51 +0800

    add factory design pattern

--- app/src/main/java/Groopr/Model/FactoryDesignForPillar/CSD.java
+++ app/src/main/java/Groopr/Model/FactoryDesignForPillar/CSD.java
@@ -1,12 +1,12 @@
 package Groopr.Model.FactoryDesignForPillar;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class CSD extends Pillar {
     public CSD(){
         super();
-        this.getTerm4ModuleList().add("50.001 Information Systems & Programming");
-        this.getTerm4ModuleList().add("50.002 Computation Structures");
-        this.getTerm4ModuleList().add("50.004 Algorithms");
-        this.getTerm5ModuleList().add("50.003 Elements of Software Construction");
-        this.getTerm5ModuleList().add("50.005 Computer System Engineering");
+        this.setTerm5ModuleList(new ArrayList<>(Arrays.asList("50.003 Elements of Software Construction","50.005 Computer System Engineering")));
+        this.setTerm4ModuleList(new ArrayList<>(Arrays.asList("50.001 Information Systems & Programming","50.002 Computation Structures","50.004 Algorithms")));
     }
 }
--- app/src/main/java/Groopr/Model/FactoryDesignForPillar/ESD.java
+++ app/src/main/java/Groopr/Model/FactoryDesignForPillar/ESD.java
@@ -1,13 +1,12 @@
 package Groopr.Model.FactoryDesignForPillar;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class ESD extends Pillar {
     public ESD(){
         super();
-        this.getTerm4ModuleList().add("40.002 Optimisation");
-        this.getTerm4ModuleList().add("40.011 Data and Business Analytics");
-        this.getTerm4ModuleList().add("40.015 Simulation Modelling and Analysis");
-        this.getTerm5ModuleList().add("40.012 Manufacturing and Service Operations");
-        this.getTerm5ModuleList().add("40.014 Engineering Systems Architecture");
-        this.getTerm5ModuleList().add("40.016 The Analytics Edge");
+        this.setTerm4ModuleList(new ArrayList<>(Arrays.asList("40.002 Optimisation","40.011 Data and Business Analytics","40.015 Simulation Modelling and Analysis")));
+        this.setTerm5ModuleList(new ArrayList<>(Arrays.asList("40.012 Manufacturing and Service Operations","40.014 Engineering Systems Architecture","40.016 The Analytics Edge")));
     }
 }
