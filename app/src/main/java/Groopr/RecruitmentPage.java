0f3a065e1fc26e1d8a186438cd55e27e03453775
Shao Ren <oonshaoren@gmail.com>
2023-03-28 20:06:18 +0800

    Added createAccount backend, made edittext visible, refactored here and there

--- app/src/main/java/Groopr/RecruitmentPage.java
+++ app/src/main/java/Groopr/RecruitmentPage.java
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.Groopr.R;
 
-public class RecruimentPage extends AppCompatActivity {
+public class RecruitmentPage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
