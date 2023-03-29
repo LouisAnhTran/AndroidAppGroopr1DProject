c0be8e87458115c174f0fc68e759d2087befce2c
Louis Anh Tran <louistrannamanh@gmail.com>
2023-03-29 13:08:32 +0800

    add functionlaity to hime page

--- app/src/main/java/Groopr/HomePage.java
+++ app/src/main/java/Groopr/HomePage.java
@@ -12,16 +12,54 @@ import com.example.Groopr.R;
 public class HomePage extends AppCompatActivity {
 
     Button myGroupsButton;
+    Button recruimentButton;
+    Button profileButton;
+    Button logoutButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_page);
         myGroupsButton = findViewById(R.id.myGroupsButton);
+        recruimentButton=findViewById(R.id.recruimentButton);
+        profileButton=findViewById(R.id.profileButton);
+        logoutButton=findViewById(R.id.logOutButton);
 
+
+        /** Navigate to recruiment page **/
+        recruimentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(HomePage.this,RecruitmentPage.class);
+                startActivity(intent);
+            }
+        });
+
+
+        /** Navigate to my group page **/
         myGroupsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent( HomePage.this, MyGroupsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        /** Navigate to profile **/
+//        profileButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent=new Intent(HomePage.this, .class);
+//                startActivity(intent);
+//            }
+//        });
+
+
+        /** Navigate to log in page**/
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(HomePage.this,MainActivity.class);
                 startActivity(intent);
             }
         });
