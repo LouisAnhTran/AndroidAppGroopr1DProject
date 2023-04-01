b5189c47e4ef9837f4dc7305954e3e74e84d3c71
Beckham <72685677+bckhm@users.noreply.github.com>
2023-04-01 15:23:23 +0800

    Added application feature
    Successfully updates Firebase. Not tested with a user logged in

--- app/src/main/java/Groopr/RecruitmentGroupInfo.java
+++ app/src/main/java/Groopr/RecruitmentGroupInfo.java
@@ -12,7 +12,10 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.Groopr.R;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -43,23 +46,16 @@ public class RecruitmentGroupInfo extends AppCompatActivity {
         grp_desc = findViewById(R.id.grp_desc);
         apply = findViewById(R.id.apply);
 
-        /** #TODO Fill up the somePage class
-        apply.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(RecruitmentGroupInfo.this, somePage.class);
-                startActivity(intent);
-            }
-        });
-         */
 
-        // Firebase RealTime Database
+        /**
+         * Loading group details from firebase
+         */
         mDatabase = FirebaseDatabase.getInstance().getReference();
         // TODO: Get ProjectID from previous page
         mDatabase.child("Project").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                projectID = "-NRfwW-3-ErA8V6LtqpC";
+                projectID = "-NRv56aotjPTxkYtPNO8";
                 Project project = snapshot.child(projectID).getValue(Project.class);
 
                 // Set details on frontend using project details
@@ -79,6 +75,52 @@ public class RecruitmentGroupInfo extends AppCompatActivity {
 
             }
         });
+
+        /** APPLYING TO A GROUP
+         * If user presses the `Apply` button
+         *  Adds current user to the pending approval list of the group
+         */
+        apply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                // Apply if user exists
+                if (user != null) {
+                    String curr_UID = user.getUid();
+                    user_apply(curr_UID, projectID);
+                }
+            }
+        });
     }
 
+    /** user_apply()
+     * helps current user apply to the selected group
+     * takes current project data from firebase
+     * updates its application list
+     */
+    void user_apply(String curr_UID, String projectID) {
+        mDatabase.child("Project").child(projectID).child("applicationsList").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                // Variables needed
+                // Get project data, and update the application list
+                ArrayList<String> application_list = (ArrayList<String>) task.getResult().getValue();
+
+
+                // Check for valid array, else create one
+                if (application_list == null) {
+                    application_list = new ArrayList<>();
+                }
+
+                // Update Application List of class
+                application_list.add(curr_UID);
+
+                // Update Firebase
+                mDatabase.child("Project").child(projectID).child("applicationsList").setValue(application_list);
+            }
+        });
+        // Update Database
+
+        }
 }
+
