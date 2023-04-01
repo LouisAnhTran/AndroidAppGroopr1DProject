36d04d5e2439cd0b7eb86e2ceca4b038bfded9c0
Beckham <72685677+bckhm@users.noreply.github.com>
2023-04-01 20:09:01 +0800

    Added checks for max group size, repeat applications, existing member

--- app/src/main/java/Groopr/RecruitmentGroupInfo.java
+++ app/src/main/java/Groopr/RecruitmentGroupInfo.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,6 +24,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import Groopr.Model.Project;
 import Groopr.Model.RecruitmentAdapter;
@@ -35,12 +37,17 @@ public class RecruitmentGroupInfo extends AppCompatActivity {
     Button apply;
 
     String projectID;
+    String curr_UID;
+    Integer group_max_size;
+    ArrayList<String> application_list;
+    List<String> member_list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recruitments_grp_info_pg);
 
+        // UI Views
         grp_name = findViewById(R.id.grp_name);
         mod_name = findViewById(R.id.mod_name);
         grp_desc = findViewById(R.id.grp_desc);
@@ -63,12 +70,16 @@ public class RecruitmentGroupInfo extends AppCompatActivity {
                 mod_name.setText(project.getModuleID());
                 grp_desc.setText(project.getMessage());
 
+                // saving attributes
+                group_max_size = project.getMaxNumberOfMember();
+                application_list = project.getApplicationsList();
+                member_list = project.getStudentList();
+
                 // Getting members
                 for (String member: project.getStudentList()) {
                     Log.d(member, member);
-                    // List view stuff
+                    // TODO: List/Recycle view stuff
                 }
-
             }
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
@@ -76,19 +87,25 @@ public class RecruitmentGroupInfo extends AppCompatActivity {
             }
         });
 
+
+
         /** APPLYING TO A GROUP
          * If user presses the `Apply` button
-         *  Adds current user to the pending approval list of the group
+         *  Adds current user to the pending approval list of the group if criterias are met:
+         *  1. User is not already pending
+         *  2. Group size is not full
          */
+
         apply.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
                 // Apply if user exists
                 if (user != null) {
-                    String curr_UID = user.getUid();
+                    curr_UID = user.getUid();
                     user_apply(curr_UID, projectID);
                 }
+                user_apply("SUP", projectID);
             }
         });
     }
@@ -96,31 +113,35 @@ public class RecruitmentGroupInfo extends AppCompatActivity {
     /** user_apply()
      * helps current user apply to the selected group
      * takes current project data from firebase
-     * updates its application list
+     * updates its application list if user isnt already in it
      */
     void user_apply(String curr_UID, String projectID) {
-        mDatabase.child("Project").child(projectID).child("applicationsList").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DataSnapshot> task) {
-                // Variables needed
-                // Get project data, and update the application list
-                ArrayList<String> application_list = (ArrayList<String>) task.getResult().getValue();
-
-
-                // Check for valid array, else create one
-                if (application_list == null) {
-                    application_list = new ArrayList<>();
-                }
-
-                // Update Application List of class
-                application_list.add(curr_UID);
-
-                // Update Firebase
-                mDatabase.child("Project").child(projectID).child("applicationsList").setValue(application_list);
-            }
-        });
-        // Update Database
-
+        String update = "";
+        // Check for valid array, else create one
+        if (application_list == null) {
+            application_list = new ArrayList<>();
         }
+
+        // Update Application List of class
+        // Checks if User exists in Array first and if group is already full
+        // Updates Toast's String according to what happened here
+        if (!application_list.contains(curr_UID) && application_list.size() < group_max_size && !member_list.contains(curr_UID)) {
+            application_list.add(curr_UID);
+            // Update Database
+            mDatabase.child("Project").child(projectID).child("applicationsList").setValue(application_list);
+            update = "Application Successful!";
+        } else if (!(member_list.size() < group_max_size)) {
+            update = "Group is full!";
+        } else if (application_list.contains(curr_UID)) {
+            update = "You have already applied!";
+        } else if (member_list.contains(curr_UID)) {
+            update = "You're already a member of this group!";
+        }
+
+        // Show toast
+        Toast toast = Toast.makeText(getApplicationContext(), update, Toast.LENGTH_SHORT);
+        toast.show();
+
+    }
 }
 
