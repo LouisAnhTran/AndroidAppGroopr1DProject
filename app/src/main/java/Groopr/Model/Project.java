3f84c481193702da09a5917e8b95d98ff43dfff2
Oon Shao Ren <oonshaoren@gmail.com>
2023-04-07 01:32:19 +0800

    Added moduleName to Project class which will store full mod name Changed modID to only store module ID Added EditGroupsActivity.java

--- app/src/main/java/Groopr/Model/Project.java
+++ app/src/main/java/Groopr/Model/Project.java
@@ -7,6 +7,8 @@ import java.util.List;
 public class Project {
     private String projectName;
     private String moduleID;
+
+    private String moduleName;
     private List<String> studentList;
     private int maxNumberOfMember;
     private String message;
@@ -39,6 +41,15 @@ public class Project {
     public void setModuleID(String moduleID) {
         this.moduleID = moduleID;
     }
+
+    public String getModuleName() {
+        return moduleName;
+    }
+
+    public void setModuleName(String moduleName) {
+        this.moduleName = moduleName;
+    }
+
 
     public List<String> getStudentList() {
         return studentList;
@@ -88,7 +99,8 @@ public class Project {
 
     public Project(String projectName, String moduleID, List<String> studentList, int maxNumberOfMember, String message, String skillNeeded, String teamLeaderID,ArrayList<String> applicationsList) {
         this.projectName = projectName;
-        this.moduleID = moduleID;
+        this.moduleID = moduleID.substring(0, 7);
+        this.moduleName = moduleID;
         this.studentList = studentList;
         this.maxNumberOfMember = maxNumberOfMember;
         this.message = message;
--- app/src/main/java/Groopr/Model/ProjectSupport.java
+++ app/src/main/java/Groopr/Model/ProjectSupport.java
@@ -3,11 +3,12 @@ package Groopr.Model;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ProjectSupport {
+public class ProjectSupport extends Project{
     private String projectName;
 
     private String projectID;
     private String moduleID;
+    private String moduleName;
     private List<String> studentList;
     private int maxNumberOfMember;
     private String message;
@@ -48,6 +49,14 @@ public class ProjectSupport {
     public void setModuleID(String moduleID) {
         this.moduleID = moduleID;
     }
+    public String getModuleName() {
+        return moduleName;
+    }
+
+    public void setModuleName(String moduleName) {
+        this.moduleName = moduleName;
+    }
+
 
     public List<String> getStudentList() {
         return studentList;
@@ -95,7 +104,8 @@ public class ProjectSupport {
 
     public ProjectSupport(String projectName, String moduleID, List<String> studentList, int maxNumberOfMember, String message, String skillNeeded, String teamLeaderID,ArrayList<String> applicationsList) {
         this.projectName = projectName;
-        this.moduleID = moduleID;
+        this.moduleID = moduleID.substring(0, 7);
+        this.moduleName = moduleID;
         this.studentList = studentList;
         this.maxNumberOfMember = maxNumberOfMember;
         this.message = message;
